cb01347d33c53f891aa107628b9c853b611982c1
eXeis-ixt <mjlickerlim56789@gmail.com>
2024-11-14 22:34:54 +0600

    Added navbar link to home (temporary)

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,9 +1,12 @@
+import Navbar from "@/Components/ui/Navbar";
 import { Head, Link } from "@inertiajs/react";
 
 export default function Home() {
     return (
         <>
             <Head title="Get Started" />
+
+            <Navbar />
 
             <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
                 <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
